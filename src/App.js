75c668f20ec5bce46a2520bf4e47dfd10446db1c
Tetianamost <60158845+Tetianamost@users.noreply.github.com>
2022-09-28 13:12:18 -0600

    installed modules for react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-center">Hello</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
